23a819cd7354bf3fb99f5789dc7de8ae22da95b5
Sarah Dean <128323898+sarahadean@users.noreply.github.com>
2023-05-31 12:25:36 -0500

    stuck on failing tests

--- lib/classes/trip.py
+++ lib/classes/trip.py
@@ -1,8 +1,57 @@
 
+#intermediary
+#Deliverables:
+# Trip __init__(self, visitor, national_park, start_date, end_date)
+#       Trips should be initialized with a visitor, national_park, start_date(str), end_date(str)
+
+#Trip property visitor
+#       Returns the visitor object for that trip
+#       Must be of type Visitor
+#       raise Exception if setter fails
+
+# Trip property national_park
+#       Returns the NationalPark object for that trip
+#       Must be of type NationalPark
+#       raise Exception if setter fails
 class Trip:
+
+    all = []
     
     def __init__(self, visitor, national_park, start_date, end_date):
         self.visitor = visitor
         self.national_park = national_park
         self.start_date = start_date
         self.end_date = end_date
+        
+        national_park.visitors(visitor) #double check these later
+        national_park.trips(self)
+
+        visitor.national_parks_visited(national_park)
+        visitor.trips(self)
+        Trip.all.append(self)
+
+    @property
+    def visitor(self):
+        return self._visitor
+    
+    @visitor.setter
+    def visitor(self, visitor):
+        from classes.visitor import Visitor
+        if isinstance(visitor, Visitor):
+            self._visitor = visitor
+        else:
+            raise Exception("Invalid")
+
+
+
+    @property
+    def national_park(self):
+        return self._national_park
+        
+    @national_park.setter
+    def national_park(self, national_park):
+        from classes.national_park import NationalPark
+        if isinstance(national_park, NationalPark):
+            self._national_park = national_park
+        else:
+            raise Exception("Invalid")
--- lib/classes/visitor.py
+++ lib/classes/visitor.py
@@ -1,12 +1,54 @@
+# Deliverables:
+# Visitor __init__(self, name)
+#       Visitor should be initialized with a name
+
+# Visitor property name
+#       Return name
+#       Names must be of type str
+#       Names must be between 1 and 15 characters, inclusive
+#       raise Exception if setter fails
+
+# Visitor trips()
+#       Returns a list of all trips for that visitor
+#       The list of trips must contain type Trip
+
+# Visitor national_parks()
+#       Returns a unique list of all parks who that visitor has visited.
+#       The list of national parks must contain type NationalPark
+
+
 class Visitor:
 
     def __init__(self, name):
         self.name = name
-        
+        self._trips = [] #trips this individual visitor has taken
+        self._national_parks_visited = [] #unique list of national parks visited - passing new nat'l park
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25 and not hasattr(self, 'name'):
+            self._name = name
+        else:
+            raise Exception("Name must be between 1 and 15 characters")
+    
+    #Returns list of all trips this visitor has taken
     def trips(self, new_trip=None):
         from classes.trip import Trip
-        pass
-    
+
+        if isinstance(new_trip, Trip):
+            self._trips.append(new_trip)
+        return [trip for trip in Trip.all if trip.visitor == self]
+    #   return self._trips
+
+
+    #Returns unique list of all national parks this visitor has visited
     def national_parks(self, new_national_park=None):
         from classes.national_park import NationalPark
-        pass
+        from classes.trip import Trip
+        if isinstance(new_national_park, NationalPark) and new_national_park not in self._national_parks_visited:
+            self._national_parks_visited.append(new_national_park)
+        return [trip.national_park for trip in self.trips()]
