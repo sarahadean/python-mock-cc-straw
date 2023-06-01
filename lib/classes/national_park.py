15a1b5b2949288c0f3f6d469354a33963680c76e
Sarah Dean <128323898+sarahadean@users.noreply.github.com>
2023-05-31 23:03:52 -0500

    all tests passed

--- lib/classes/national_park.py
+++ lib/classes/national_park.py
@@ -19,3 +19,35 @@ class NationalPark:
         self.name = name
         self._trips = [] #list of trips planned for this nat'l park
         self._visitors = [] #unique list of visitors this nat'l park has rcvd
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if not hasattr(self, 'name') and isinstance(name, str):
+            self._name = name
+        else:
+            raise Exception
+    
+    def trips(self, new_trip=None):
+        from classes.trip import Trip
+        if new_trip and isinstance(new_trip, Trip):
+            self._trips.append(new_trip)
+        return self._trips
+    
+    def visitors(self, new_visitor=None):
+        from classes.visitor import Visitor
+        if new_visitor and isinstance(new_visitor, Visitor):
+            self._visitors.append(new_visitor)
+        return list(set(self._visitors))
+    
+    def total_visits(self):
+        return len(self._trips)
+    
+    #returns the visitor who has visited the park the most
+    #[Joe, Jay, Tom, Joe, Sam, Joe]
+    # max(set(List), key = List.count)
+    def best_visitor(self):
+        return max(set(self._visitors), key=self._visitors.count)
--- lib/classes/visitor.py
+++ lib/classes/visitor.py
@@ -25,4 +25,25 @@ class Visitor:
         self._trips = [] #trips this individual visitor has taken
         self._national_parks = [] #unique list of national parks visited - passing new nat'l park
 
-   
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if not hasattr(self, 'name') and isinstance(name, str) and 1 <= len(name) <= 15:
+            self._name = name
+        else:
+            raise Exception
+        
+    def trips(self, new_trip=None):
+        from classes.trip import Trip
+        if new_trip and isinstance(new_trip, Trip):
+            self._trips.append(new_trip)
+        return self._trips
+    
+    def national_parks(self, new_national_park=None):
+        from classes.national_park import NationalPark
+        if new_national_park and isinstance(new_national_park, NationalPark):
+            self._national_parks.append(new_national_park)
+        return list(set(self._national_parks))
